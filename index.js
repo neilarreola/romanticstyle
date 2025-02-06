6ab0c86839683b0df9ebe9b707138a8a4c051c8e
Dylan Neil Garcia Arreola <dylanneil21@gmail.com>
2025-02-06 02:25:58 -0600

    First Version of App

---
+++ index.js
@@ -0,0 +1,51 @@
+const frases = [
+  "Por que le das que no!!",
+  "Mejor dale que si, ándale",
+  "Esta compu se autodestruirá en 10,9,8...",
+  "Mi cora",
+  "El verde se ve mejor",
+  "Ahhhhhhhhhhhhhhh",
+  "Esto va a explotar!!",
+  "No me duele, me quema, me lastimaaa",
+  ""
+]
+let n = 0;
+// Obtener el botón por su ID
+const botonNo = document.getElementById('miBotonNo');
+const botonYes = document.getElementById('miBotonYes');
+// Añadir el evento de clic
+botonNo.addEventListener('click', function() {
+  
+  // Obtener el tamaño actual del botón
+    const ancho = botonNo.offsetWidth;  // Obtiene el ancho del botón
+    const alto = botonNo.offsetHeight;  // Obtiene la altura del botón
+    console.log(ancho);
+    console.log(alto);
+    botonNo.style.width = (ancho + 70) + 'px';
+    botonNo.style.height = (alto + 30) + 'px';
+    botonNo.innerText = frases[n];
+    
+    if ((n+1) == frases.length) {
+      n = 0;
+    } else {
+      n = n+1;
+    }
+});
+
+botonYes.addEventListener('click', function() {
+   // Mostrar el contenedor del video
+   videoContainer.style.display = 'block';
+   // Reproducir el video
+   miVideo.play();
+
+
+  //OCULTAR TODO
+  botonNo.style.display = "none";
+  botonYes.style.display = "none";
+  document.getElementById("img").style.display = "none";
+  document.getElementById("question").style.display = "none";
+
+});
+
+
+
