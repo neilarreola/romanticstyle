979a489ce169a416f20558a12e925c0337c88792
Dylan Neil Garcia Arreola <dylanneil21@gmail.com>
2025-02-06 13:43:36 -0600

    Version 2

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const frases = [
-  "Por que le das que no!!",
-  "Mejor dale que si, ándale",
+  "Por que le das que no :(",
+  "El verde se ve mejor, ahí nomas lo dejo...",
   "Esta compu se autodestruirá en 10,9,8...",
-  "Mi cora",
-  "El verde se ve mejor",
+  "Mi cora </3",
+  "A veces la vida nos es como queremos ",
   "Ahhhhhhhhhhhhhhh",
   "Esto va a explotar!!",
-  "No me duele, me quema, me lastimaaa",
-  ""
+  "No me duele, me quema, me lastimaaa :(",
+  "Dios ven por mí",
+  "Cada vez que le das que no, dejas sin hogar a un dylan.",
+  "Ya no es graciosoooo",
+  "Se me acabaron las frases, dale que sí!"
 ]
 let n = 0;
 // Obtener el botón por su ID
@@ -24,9 +27,11 @@ botonNo.addEventListener('click', function() {
     botonNo.style.width = (ancho + 70) + 'px';
     botonNo.style.height = (alto + 30) + 'px';
     botonNo.innerText = frases[n];
-    
+    png.style.display = "none";
+    gif.style.display = "block";
+
     if ((n+1) == frases.length) {
-      n = 0;
+      n = frases.length - 1;
     } else {
       n = n+1;
     }
